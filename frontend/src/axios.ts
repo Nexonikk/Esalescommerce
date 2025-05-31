b6637226c5a2e6f444dc7e220fa229c54ecb6234
Yusuf <143870776+Nexonikk@users.noreply.github.com>
2025-06-01 01:14:17 +0530

    added backend url to axios service.

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosBase = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "esalescommerce-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
